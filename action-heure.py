5c4eb2ec8c07c5734ea5f5231e5698d7882f4ac0
Joseph Dureau <joseph.dureau@snips.ai>
2018-09-10 00:03:49 +0200

    more logs

--- action-heure.py
+++ action-heure.py
@@ -47,7 +47,8 @@ def intent_received(hermes, intent_message):
 
 	print(slots.assistant_name)
 	print(len(slots.assistant_name))
-	print(intent_message.intent.intent.probabilty)
+	print(type(intent_message.intent))
+	print(intent_message.intent.probabilty)
 
 	if intent_message.intent.intent_name == 'Joseph:askTime' and len(slots.assistant_name) > 0:
 
